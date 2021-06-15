12fbfc09e3cc5a49c22180a9d74f64574f1e15f0
rks-ookuma <takahiro.okuma@rakus.co.jp>
2021-06-15 11:55:34 +0900

    first commit

---
+++ src/main/java/jp/co/example/ecommerce_b/ServletInitializer.java
@@ -0,0 +1,13 @@
+package jp.co.example.ecommerce_b;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Ec202104bApplication.class);
+	}
+
+}
